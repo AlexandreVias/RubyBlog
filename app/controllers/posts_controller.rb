0b164d4ef97df5e1361b51eddc1ddd014617f26a
Alexandre <alexandre.vias@hotmail.fr>
2019-07-03 17:03:25 +0200

    new post current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def index
     @posts = Post.all.limit(5)
     @count = @posts.count
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build post_attributes
+    if @post.save
+      redirect_to [current_user, @post]
+    else
+      render :new
+    end
+  end
+
+  def post_attributes
+    params[:post].permit(:title, :kicker, :body)
   end
 end
--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -6,28 +6,12 @@ class Users::PostsController < ApplicationController
     @count = @posts.count
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = @user.posts.build post_attributes
-    if @post.save
-      redirect_to [@user, @post]
-    else
-      render :new
-    end
-  end
-
   def show
-    @post = Post.find(params[:id])
+    @user.posts.find(params[:id])
   end
 
   def load_user
     @user = User.find(params[:user_id])
   end
 
-  def post_attributes
-    attributes = params[:post].permit(:title, :kicker, :body)
-  end
 end
