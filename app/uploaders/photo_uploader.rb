d8f36b6aa56d4f1efd78b8e0b1e8cda27adabe91
Baume Aurore <aurore.baume@etu.u-bordeaux.fr>
2019-07-05 11:58:19 +0200

    upload image is OK

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,6 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
