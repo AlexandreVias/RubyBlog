e639477659d36a5bac60c3505d83d8df71675890
Alexandre <alexandre.vias@hotmail.fr>
2019-07-05 11:57:28 +0200

    access edit

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,4 +1,5 @@
 class Users::PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :load_user
 
   def index
@@ -10,8 +11,26 @@ class Users::PostsController < ApplicationController
     @post = @user.posts.find(params[:id])
   end
 
+  def edit
+    if current_user.id == @user.id
+      @post = @user.posts.find(params[:id])
+    else
+      redirect_to user_post_path
+    end
+  end
+
+  def update
+    if current_user.id == @user.id and @user.post.update post_attributes
+
+    end
+  end
+
   def load_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_attributes
+    params[:post].permit(:title, :kicker, :body)
   end
 
 end
