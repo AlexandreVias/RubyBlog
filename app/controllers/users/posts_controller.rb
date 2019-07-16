a534d7d695315015d1df61d46aced79b97cb32c0
Alexandre <alexandre.vias@hotmail.fr>
2019-07-16 20:40:19 +0200

    fix update, add destroy, move list user posts

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,6 +1,7 @@
 class Users::PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update]
   before_action :load_user
+  before_action :load_post, only: [:update, :destroy]
 
   def index
     @posts = @user.posts
@@ -20,13 +21,26 @@ class Users::PostsController < ApplicationController
   end
 
   def update
-    if current_user.id == @user.id and @user.post.update post_attributes
-
+    if current_user.id == @user.id and @post.update post_attributes
+      redirect_to user_post_path
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    if current_user.id == @user.id
+      @post.destroy
+    end
+    redirect_to user_posts_path
   end
 
   def load_user
     @user = User.find(params[:user_id])
+  end
+
+  def load_post
+    @post = @user.posts.find(params[:id])
   end
 
   def post_attributes
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,3 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: %i[show]
-
-  def load_user
-    @user = User.find(params[:id])
-  end
 
 end
