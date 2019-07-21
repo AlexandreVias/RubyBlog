80833b396fd4ea5a9c6141ce9b50c2bb32e7a7a8
Aurore Baumé <baume.aurore@gmail.com>
2019-07-21 21:54:42 +0200

    update pictures

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -30,11 +30,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [50, 30]
-  end
-
-  version :banner do
-    process resize_to_fill: [500, 200]
+    process resize_to_fill: [300, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
