2a5b90f796b838ea4da53153a4c7a5b058ab6507
Alexandre <alexandre.vias@hotmail.fr>
2019-07-21 20:45:29 +0200

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,28 +3,25 @@ class CommentsController < ApplicationController
   before_action :load_user, only: [:create]
   before_action :load_post, only: [:create]
 
-
   def create
-    @comment = @post.comments.build comment_attributes
-    @comment.user_id = current_user
+    comment = @post.comments.build comment_attributes
+    comment.user_id = current_user.id
 
-    if @comment.save
-      redirect_to post
-    else
+    comment.save
 
-    end
+    redirect_to [current_user, @post]
   end
 
   def comment_attributes
     params[:comment].permit(:body)
   end
 
-  def load_user
-    @user = User.find(params[:user_id])
-  end
-
   def load_post
     @post = @user.posts.find(params[:post_id])
+  end
+
+  def load_user
+    @user = User.find(params[:user_id])
   end
 
 end
